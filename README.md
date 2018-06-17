d717e7cf50bd562152fea46041ceb477aa2d15db
Leticia Verta <leticia.verta@gmail.com>
2018-06-17 12:17:53 -0300

    Connected to Cockroach

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # go-api-test
 
+## SETUP
+
+- Install [Cockroach DB](https://www.cockroachlabs.com/docs/stable/install-cockroachdb.html)
+
+- Start database
+```
+cockroach start --insecure
+```
+
+- Create an user
+```
+cockroach user set myuser --insecure
+```
+
+- Create database
+```
+cockroach sql --insecure -e 'CREATE DATABASE wwg'
+```
+
+- And create table(s)
+```
+create table musics (id serial primary key, artist string, title string);
+```
+
+## RUN
 `go run ./main.go` :poop:
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"encoding/json"
-	"net/http"
+	"database/sql"
 	"io/ioutil"
+	"net/http"
 
 	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
 )
 
 func main() {
@@ -22,24 +24,30 @@ type Music struct {
 	Artist string
 }
 
-var musics = []Music{
-	Music{Title: "Brain", Artist: "Banks"},
-	Music{Title: "Come Down", Artist: "Anderson .Paak"},
-}
-
 func getMusics(w http.ResponseWriter, r *http.Request) {
+	var musics []Music
+
+	connectionString := "postgresql://wwg@localhost:26257/wwg?sslmode=disable"
+	db, _ :=  sql.Open("postgres", connectionString)
+	rows, _ := db.Query("SELECT artist, title FROM musics")
+    defer rows.Close()
+    for rows.Next() {
+        var artist, title string
+        rows.Scan(&artist, &title);
+        musics = append(musics, Music{Title: title, Artist: artist})
+    }
+
 	json.NewEncoder(w).Encode(musics)
 }
 
 func createMusic(w http.ResponseWriter, r *http.Request) {
 	var music Music
-	body, err := ioutil.ReadAll(r.Body)
-	if err != nil {
-		panic(err)
-	}
-
+	body, _ := ioutil.ReadAll(r.Body)
 	json.Unmarshal(body, &music);
-	musics = append(musics, music)
+
+	connectionString := "postgresql://wwg@localhost:26257/wwg?sslmode=disable"
+	db, _ :=  sql.Open("postgres", connectionString)
+	db.Exec("INSERT INTO musics (title, artist) VALUES ('" + music.Title + "', '" + music.Artist + "')");
 
 	w.WriteHeader(http.StatusCreated)
 	json.NewEncoder(w).Encode(music)
